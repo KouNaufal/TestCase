eca95857029be89fea72c468b8c6a8e8c68470ee
KouNaufal <naufalkabo157@gmail.com>
2023-06-12 15:04:41 +0700

    First Commit

---
+++ Scripts/Page Register/Register Succes/Script1686557030881.groovy
@@ -0,0 +1,38 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://pos-dev.flavours.sa/sign-up')
+
+WebUI.setText(findTestObject('Page_Flavours POS/Page_Flavours POS/input_Username_username'), 'tester8')
+
+WebUI.setText(findTestObject('Object Repository/Page_Flavours POS/input_Email Address_email'), 'tester8@gmail.com')
+
+WebUI.setText(findTestObject('Object Repository/Page_Flavours POS/input_Phone Number_phone'), '588888888')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_Flavours POS/input_Password_password'), 'meZQjHK4FPM=')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_Flavours POS/input_Confirm Password_password_confirmation'), 
+    'meZQjHK4FPM=')
+
+WebUI.click(findTestObject('Object Repository/Page_Flavours POS/button_Register_1'))
+
+WebUI.verifyElementText(findTestObject('Page_Flavours POS/div_SuccessSign Up Success'), '')
+
